0ded44ead495e1414f8d647bb5dff44e8ba2142c
Chandrasekhar Anem <chandu.msme@gmail.com>
2019-01-21 22:10:50 +0530

    Admin views

--- auth_system/accounts/admin.py
+++ auth_system/accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserAdminChangeForm, UserAdminCreationForm
+User = get_user_model()
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """
+    User Admin class for viewing the Users
+    """
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ('email', 'admin', 'staff', 'is_active')
+    list_filter = ('admin', 'staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('admin', 'staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name')}
+         ),
+    )
+    search_fields = ('email', 'first_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
