ac0fc3fe51ed5dcc8de60f093016f2398b61d999
Chandrasekhar Anem <chandu.msme@gmail.com>
2019-01-21 22:16:50 +0530

    FORM Views [Done]

--- auth_system/accounts/forms.py
+++ auth_system/accounts/forms.py
@@ -3,3 +3,70 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from .models import User
 
 
+class RegisterForm(forms.ModelForm):
+    """
+    form class for normal user registration. Inherited from ModelForm
+    """
+    email = forms.EmailField(widget=forms.EmailInput
+                           (attrs={'placeholder': 'Enter your email'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User    # model
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    """
+    As superuser can create superusers, this form will be used for Admin user creation.
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    """
+    Form for changing the User Admin
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password', 'is_active', 'staff', 'admin')
+        # fields that could be edited
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+class LoginForm(forms.Form):
+    # Login form
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput)
